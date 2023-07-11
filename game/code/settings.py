05108be73302cba1a1c88a0f1bf3cd103b860c1b
ksusha <ktsykanova@yandex.ru>
2023-07-11 13:43:11 +0300

    update settings

--- game/code/settings.py
+++ game/code/settings.py
@@ -1,3 +1,8 @@
+from tools import ImgEditor
+
+NAME = 'AbobaGame'
+ICON = ImgEditor.load_image('icon.png', colorkey=-1)
+
 SIZE = WIDTH, HEIGHT = 1536, 864
 CENTER = WIDTH // 2, HEIGHT // 2
 
@@ -5,17 +10,6 @@ INTERACTION_DISTANCE = 120
 
 DIALOGUE_POS = (CENTER[0], 750)
 MAX_DIALOGUE_LENGTH = 111
-DIALOGUES = {
-    'пугало': ['denis_Это пугало как будто говорит воронам: "Страдай или вали".',
-               'denis_Зачем ставить пугало рядом с клумбой?'],
-    'указатель': ['base_"Торговец".', 'denis_Надпись почти не разобрать.'],
-    'корзинка для пикника': ['denis_Блин, пустая.'],
-    'клумба': [
-        'denis_Не могу узнать ни один цветок.', 'denis_Земля ещё сырая. Кто-то за ней ухаживает... Интересно, кто.'],
-    'окно': ['denis_Ничего не видно.'],
-    'почтовый ящик': ['denis_Хм... пусто.'],
-    'коврик': ['denis_Под ковриком лежат ключи... Один от двери, от чего другие?'],
-}
 
 FONT = '../assets/fonts/main.ttf'
 TEXT_COLOR = (97, 57, 34)
